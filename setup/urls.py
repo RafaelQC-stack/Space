e2698c9eae3e175191462270765b2e275213e265
RafaelQC <rafaelqc@rrows019003.tre-ro.jus.br>
2025-10-23 14:29:17 -0400

    commit de esqueleto pronto

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('',include('galeria.urls')),
 
 
-]
+]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
